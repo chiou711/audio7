5511c9e8dc512464ca45a28c889468a88b533eba
chiou711 <chiou711@gmail.com>
2022-07-26 19:08:42 +0800

    2022-07-26 fix URL with special character

--- app/src/main/java/com/cw/audio7/util/image/ImageFetcher.java
+++ app/src/main/java/com/cw/audio7/util/image/ImageFetcher.java
@@ -32,10 +32,11 @@ import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import androidx.multidex.BuildConfig;
 
@@ -315,6 +316,9 @@ public class ImageFetcher extends ImageResizer {
         BufferedOutputStream out = null;
         BufferedInputStream in;
 
+        if(!isValidURL(urlString))
+            return false;
+
         mmr = new MediaMetadataRetriever();
         mmr.setDataSource(context, Uri.parse(urlString));
 
@@ -355,5 +359,37 @@ public class ImageFetcher extends ImageResizer {
             } catch (final IOException e) {}
         }
         return false;
+    }
+
+
+    // Function to validate URL
+    // using regular expression
+    public static boolean
+    isValidURL(String url)
+    {
+        // Regex to check valid URL
+        String regex = "((http|https)://)(www.)?"
+                + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+                + "{2,256}\\.[a-z]"
+                + "{2,6}\\b([-a-zA-Z0-9@:%"
+                + "._\\+~#?&//=]*)";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the string is empty
+        // return false
+        if (url == null) {
+            return false;
+        }
+
+        // Find match between given string
+        // and regular expression
+        // using Pattern.matcher()
+        Matcher m = p.matcher(url);
+
+        // Return if the string
+        // matched the ReGex
+        return m.matches();
     }
 }
